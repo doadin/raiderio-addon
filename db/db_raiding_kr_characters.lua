4a5104e02987406c43aafddafd784755bc104350
Vladinator89 <vladix@gmail.com>
2022-08-21 12:11:25 +0200

    - Adjusted project settings to prevent scanning the db files.
    - Using the testing v2 dataset.
    
    - Adjusted several lines of code to rely on the new profile `raidProgress` property that will be used for this new way of summarizing raid progress information.

--- db/db_raiding_kr_characters.lua
+++ db/db_raiding_kr_characters.lua
@@ -2,9 +2,10 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={["name"]=...,["data"]=2,["region"]="kr",["date"]="2022-08-14T03:12:26Z",["numCharacters"]=521,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10},[2]={["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10},[3]={["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSOTFO",["bossCount"]=11}},["previousRaids"]={[1]={["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11},[2]={["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10},[3]={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10}},["db"]={}}
+local provider={["name"]=...,["data"]=2,["region"]="kr",["date"]="2022-08-20T20:28:15Z",["numCharacters"]=67899,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10,["ordinal"]=1},[2]={["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10,["ordinal"]=2},[3]={["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSFO",["bossCount"]=11,["ordinal"]=3}},["previousRaids"]={[1]={["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11,["ordinal"]=4},[2]={["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10,["ordinal"]=5},[3]={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10,["ordinal"]=6}},["db"]={}}
 local F
 
+F = function() provider.db["알렉스트라자"]={0,"건새우어묵볶음","검둥아달려","검둥아달료","너하나만을","눈알초점상실","댄나이","데빌데몬","둥둥아달료","드드","드렁큰라이거","드루이드트리오나","드루이브","라끄시안","로드바이크","로드파이터","마법사트리오나","미쿡소","박별섬","반살","밴시","벤구리온","보라정구지","블러드죽움의기사","사냥꾼트리오나","상어","성기사트리오나","쉬엔","용돈좀마누라","율리안나","은빛표범","이름짓는데삼십분","이쁜이누님","쟌다르크","정구지","주술사트리오나","죽기트리오나","진애","카이덴","코아미","탈모인","트리오나","파란정구지","파이크","하늘냥꾸니","하늘주수리","한국쇠고기","핫셀블러드","현건","현설","현시","홀로짜는젖"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_raiding_tw_lookup.lua
+++ db/db_raiding_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={["name"]=...,["data"]=2,["region"]="tw",["date"]="2022-08-14T03:12:26Z",["numCharacters"]=521,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10},[2]={["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10},[3]={["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSOTFO",["bossCount"]=11}},["previousRaids"]={[1]={["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11},[2]={["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10},[3]={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10}},["lookup"]={}}
+local provider={["name"]=...,["data"]=2,["region"]="tw",["date"]="2022-08-20T20:28:15Z",["numCharacters"]=67899,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10,["ordinal"]=1},[2]={["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10,["ordinal"]=2},[3]={["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSFO",["bossCount"]=11,["ordinal"]=3}},["previousRaids"]={[1]={["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11,["ordinal"]=4},[2]={["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10,["ordinal"]=5},[3]={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10,["ordinal"]=6}},["lookup"]={}}
 local F
 
 -- chunk size: 0
