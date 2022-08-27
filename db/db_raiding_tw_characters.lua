d6af43f3c1b7beb614845ec4d55ed22a045a68e3
Vladinator89 <vladix@gmail.com>
2022-08-27 03:46:38 +0200

    - Updated DB files.
    - Future proofing raid locale name, in case we add support for it.
    
    - Added raid related util methods.
    
    - Updated the util `GetLFDStatus`, `GetInstanceStatus` and `GetLFDStatusForCurrentActivity` methods to support the raid activities.
    
    - Added fate related methods to util module. `IsRaidFated` and `GetFatedRaids`.
    
    - Loading the raid provider we will dynamically assign the `dungeon` to the relevant raid using the `id` value.
    
    - Hotfixed fated raid `id`'s to refer to the actual raid `id` so the lookup functions, instead of using the internal `id` that leads nowhere. Also fixed the `ordinal` for the current raids so the order is correct.
    
    - `SummarizeRaidProgress` will mark if the raid is current or not, as a way to detect if its origin was from the current or previous tables.
    
    - We ensure that `GetBestRunOfDungeons` doesn't return the best dungeon if it's missing the `keystone_instance` property, that is missing for raid entries, so we skip those.
    
    - Implemented `AppendRaidProfileToTooltip` and `AppendRaidProgressToTooltip` that are called from `UpdateTooltip` in the render module to actually output the raid profile, or raid progress in the tooltip.

--- db/db_raiding_tw_characters.lua
+++ db/db_raiding_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={["name"]=...,["data"]=2,["region"]="tw",["date"]="2022-08-20T20:28:15Z",["numCharacters"]=67899,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10,["ordinal"]=1},[2]={["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10,["ordinal"]=2},[3]={["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSFO",["bossCount"]=11,["ordinal"]=3}},["previousRaids"]={[1]={["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11,["ordinal"]=4},[2]={["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10,["ordinal"]=5},[3]={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10,["ordinal"]=6}},["db"]={}}
+local provider={["name"]=...,["data"]=2,["region"]="tw",["date"]="2022-08-26T13:36:35Z",["numCharacters"]=67611,["recordSizeInBytes"]=35,["encodingOrder"]={[1]=1,[2]=2,[3]=3,[4]=4},["currentRaids"]={[1]={["id"]=100013224,["name"]="Fated Castle Nathria",["shortName"]="FCN",["bossCount"]=10,["ordinal"]=1},[2]={["id"]=100013561,["name"]="Fated Sanctum of Domination",["shortName"]="FSOD",["bossCount"]=10,["ordinal"]=2},[3]={["id"]=100013742,["name"]="Fated Sepulcher of the First Ones",["shortName"]="FSFO",["bossCount"]=11,["ordinal"]=3}},["previousRaids"]={[1]={["id"]=13742,["name"]="Sepulcher of the First Ones",["shortName"]="SFO",["bossCount"]=11,["ordinal"]=4},[2]={["id"]=13561,["name"]="Sanctum of Domination",["shortName"]="SOD",["bossCount"]=10,["ordinal"]=5},[3]={["id"]=13224,["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10,["ordinal"]=6}},["db"]={}}
 local F
 
 
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-07-13T07:29:39Z",numCharacters=15008,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-08-26T14:35:29Z",numCharacters=17628,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
-F = function() provider.lookup[1] = "v\14" end F()
+F = function() provider.lookup[1] = "\8\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
