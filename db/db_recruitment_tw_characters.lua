d5437d27f0393609de6fca417a8b5a5fb6e874ac
Vladinator89 <vladix@gmail.com>
2022-08-28 16:37:52 +0200

    - The new raid `id` is the actual id reference (with the `mapId` being the previous internal `id` value) so we just use this to look up the raid from the `ns.raids` table.
    - Updated `AppendRaidProfileToTooltip` to consider LFD and Fate state to colorize the raid header properly.
    
    - Updated the db files with the latest test data.

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-08-26T14:35:29Z",numCharacters=17628,db={}}
+local provider={name=...,data=3,region="tw",date="2022-08-27T03:39:39Z",numCharacters=17592,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Spades"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-08-26T14:35:29Z",numCharacters=17628,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-08-27T03:39:39Z",numCharacters=17592,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
