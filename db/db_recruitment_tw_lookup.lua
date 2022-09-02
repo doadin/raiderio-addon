3bd385605a229cf094cf3b7a43f1ecc5122e2f44
Vladinator89 <vladix@gmail.com>
2022-09-02 18:13:03 +0200

    - Updated db to latest alpha set.
    - Updated readme API documentations.
    
    - Fixed bug with search UI not respecting the region when creating profile URL.

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-08-29T01:27:36Z",numCharacters=17420,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-09-02T13:45:58Z",numCharacters=16774,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
-F = function() provider.lookup[1] = "\8\4" end F()
+F = function() provider.lookup[1] = "\14\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
