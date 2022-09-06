935856d0f8322af2a1aee0438e1ad9f4aa1600e4
Vladinator89 <vladix@gmail.com>
2022-09-06 17:30:26 +0200

    - Added support if `killsPerBoss` isn't available we rely on `progressCount` to backfill the data structure so we can still show the total X/Y raid progress on the difficulty, even if the exact boss information is lost it's enough for the tooltip to be shown. Due to this we also have to add the checks to avoid double-dipping figures by only matching up current raids with the current data, or the older raids with the previous progress data.
    - Updated using the latest alpha dataset.

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-09-05T02:49:57Z",numCharacters=16714,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-09-06T14:59:25Z",numCharacters=16791,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
