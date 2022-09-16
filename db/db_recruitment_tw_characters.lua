3d40f728bcdb5d93f198c539f5638dc22b70b64a
Vladinator89 <vladix@gmail.com>
2022-09-16 22:20:07 +0200

    The scrollbox OnUpdate event will occur after frames are populated. We use it to detect changes.

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,10 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-09-15T15:43:46Z",numCharacters=16138,db={}}
+local provider={name=...,data=3,region="tw",date="2022-09-16T19:37:19Z",numCharacters=16069,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Spades"} end F()
+F = function() provider.db["屠魔山谷"]={2,"胖達莉亞"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-09-15T15:43:46Z",numCharacters=16138,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-09-16T19:37:19Z",numCharacters=16069,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 2
-F = function() provider.lookup[1] = "\12\4" end F()
+-- chunk size: 4
+F = function() provider.lookup[1] = "\12\4\20\16" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
