bf5c3d3fc3a2116392c0b061089b939820096d0e
Jah Raphael <jahraphael@yahoo.com>
2022-11-29 09:56:46 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-11-28T09:48:38Z",numCharacters=17831,db={}}
+local provider={name=...,data=3,region="tw",date="2022-11-29T09:47:22Z",numCharacters=16461,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Acinduction","Virgil","冬瓜茶","娜雅妮","小喵德","御煌","絕版小嗚","馬來糕"} end F()
-F = function() provider.db["語風"]={16,"Gnomeblood","Gnomebrew","李知恩親手","那芙"} end F()
+F = function() provider.db["語風"]={16,"Gnomeblood","Gnomebrew","Gnomehowler","那芙"} end F()
 F = function() provider.db["眾星之子"]={24,"Gnomefaker"} end F()
 F = function() provider.db["冰風崗哨"]={26,"盧十三","破日","美特菈"} end F()
 F = function() provider.db["夜空之歌"]={32,"Alyssa","Hygieia","廣島胖丁","東京胖丁","眸眸"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-11-28T09:48:38Z",numCharacters=17831,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-11-29T09:47:22Z",numCharacters=16461,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 42
