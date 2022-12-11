4ae151cdf480666f7a8ef818cb16c792a5a23cd3
Jah Raphael <jahraphael@yahoo.com>
2022-12-11 07:36:44 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,13 +2,12 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-10T07:32:48Z",numCharacters=15751,db={}}
+local provider={name=...,data=3,region="tw",date="2022-12-11T07:30:41Z",numCharacters=16035,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Acinduction","Virgil","冬瓜茶","娜雅妮","小喵德","御煌","馬來糕"} end F()
-F = function() provider.db["語風"]={14,"Gnomeblood","Gnomebrew","Gnomehowler","那芙"} end F()
-F = function() provider.db["眾星之子"]={22,"Gnomefaker"} end F()
-F = function() provider.db["冰風崗哨"]={24,"破日"} end F()
+F = function() provider.db["語風"]={14,"Gnomeblood","Gnomehowler","那芙"} end F()
+F = function() provider.db["眾星之子"]={20,"Gnomefaker"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-10T07:32:48Z",numCharacters=15751,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-12-11T07:30:41Z",numCharacters=16035,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 26
-F = function() provider.lookup[1] = "?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5\8\4" end F()
+-- chunk size: 22
+F = function() provider.lookup[1] = "?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
