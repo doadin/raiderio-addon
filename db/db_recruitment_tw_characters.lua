888e84752a99b726421e1feb5a20a219ad93054a
Jah Raphael <jahraphael@yahoo.com>
2022-12-14 05:29:01 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,11 +2,13 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-12T07:28:35Z",numCharacters=16778,db={}}
+local provider={name=...,data=3,region="tw",date="2022-12-14T05:28:00Z",numCharacters=17067,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Virgil","冬瓜茶","娜雅妮","小喵德","御煌","馬來糕"} end F()
-F = function() provider.db["語風"]={12,"Gnomeblood","Gnomehowler","那芙"} end F()
+F = function() provider.db["語風"]={12,"Gnomeblood","Gnomebrew","Gnomehowler","那芙"} end F()
+F = function() provider.db["日落沼澤"]={20,"一條龍服務","冰火雙重天","心有灵犀","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","阿戦","非正义的圣光"} end F()
+F = function() provider.db["憤怒使者"]={38,"扛麥郎習近平","鹹魚餅"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
