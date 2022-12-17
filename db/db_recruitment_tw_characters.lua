1ff8c177eb98b80cd6bd2849964bd7d008acf21c
Jah Raphael <jahraphael@yahoo.com>
2022-12-17 06:45:35 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-16T06:29:35Z",numCharacters=17779,db={}}
+local provider={name=...,data=3,region="tw",date="2022-12-17T06:40:11Z",numCharacters=18505,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Arddream","Killua","Virgil","一颗豆芽呀","冬瓜茶","娜雅妮","小喵德","御煌","水月龙芯","牛爱草","諾米化身為龍","賓尼兎","赫爾墨斯丶墮","邪刃肥貓","馬來糕"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-16T06:29:35Z",numCharacters=17779,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-12-17T06:40:11Z",numCharacters=18505,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 62
