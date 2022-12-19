1151a29220242f1690ea45901715c7bd819f2eab
Jah Raphael <jahraphael@yahoo.com>
2022-12-19 06:45:53 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2022-12-18T18:33:37Z",numCharacters=19175,db={}}
+local provider={name=...,data=3,region="kr",date="2022-12-19T06:41:24Z",numCharacters=19740,db={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"민트초코떡볶이","민트초코불고기"} end F()
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-18T18:33:37Z",numCharacters=19175,db={}}
+local provider={name=...,data=3,region="tw",date="2022-12-19T06:41:24Z",numCharacters=19740,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Acinduction","Arddream","Killua","Virgil","一颗豆芽呀","冬瓜茶","娜雅妮","小喵德","御煌","水月龙芯","牛爱草","芋粿","諾米化身為龍","賓尼兎","赫爾墨斯丶墮","邪刃肥貓","馬來糕"} end F()
