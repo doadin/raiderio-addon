371e96309889acc43f95723eb614d218fca44bdd
Jah Raphael <jahraphael@yahoo.com>
2022-12-24 07:15:55 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,14 +2,15 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-23T06:55:54Z",numCharacters=21915,db={}}
+local provider={name=...,data=3,region="tw",date="2022-12-24T07:03:43Z",numCharacters=21980,db={}}
 local F
 
 F = function() provider.db["日落沼澤"]={0,"一條龍服務","冰火雙重天","心有靈犀","我爱看烟花","灵犀","無尾熊之星","練個喚能師","肉嘟嘟胖呼呼","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
 F = function() provider.db["暗影之月"]={22,"Arddream","Killua","一颗豆芽呀","水月龙芯","滿玥丶","牛爱草","諾米化身為龍","賓尼兎","赫爾墨斯丶墮","邪刃肥貓","野馬丶"} end F()
-F = function() provider.db["憤怒使者"]={44,"扛麥郎習近平","鹹魚餅"} end F()
-F = function() provider.db["語風"]={48,"Solozwarlock"} end F()
-F = function() provider.db["水晶之刺"]={50,"Mikha"} end F()
+F = function() provider.db["阿薩斯"]={44,"Acsc","Zetaker"} end F()
+F = function() provider.db["憤怒使者"]={48,"扛麥郎習近平","鹹魚餅"} end F()
+F = function() provider.db["語風"]={52,"Solozwarlock"} end F()
+F = function() provider.db["水晶之刺"]={54,"Mikha"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2022-12-23T06:55:54Z",numCharacters=21915,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2022-12-24T07:03:43Z",numCharacters=21980,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 52
-F = function() provider.lookup[1] = "?\29?\29?\29?\29?\29?\29\4\4?\29?\29?\29?\29?\9?\9?\9?\9\169\6?\9?\9?\9?\9?\9\169\6;\16;\16;\4\4\4" end F()
+-- chunk size: 56
+F = function() provider.lookup[1] = "?\29?\29?\29?\29?\29?\29\4\4?\29?\29?\29?\29?\9?\9?\9?\9\169\6?\9?\9?\9?\9?\9\169\6\5\16\5\4;\16;\16;\4\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
