83fecf215a681f925d801a305379c0b411f1a47e
Jah Raphael <jahraphael@yahoo.com>
2023-01-12 08:16:19 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-01-11T07:43:04Z",numCharacters=30146,db={}}
+local provider={name=...,data=3,region="tw",date="2023-01-12T07:49:21Z",numCharacters=30391,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Arddream","Killua","一颗豆芽呀","三杯老鼠肉","凝音","水月龙芯","流砂","滿玥丶","炫幻","牛爱草","諾米化身為龍","賓尼兎","赫爾墨斯丶墮","邪刃肥貓","野馬丶"} end F()
@@ -12,8 +12,9 @@ F = function() provider.db["語風"]={44,"幽蘭花","福克斯"} end F()
 F = function() provider.db["阿薩斯"]={48,"四葉未來","沒有你的未來"} end F()
 F = function() provider.db["眾星之子"]={52,"舞天曦"} end F()
 F = function() provider.db["日落沼澤"]={54,"一條龍服務","冰火雙重天","心有靈犀","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","蛇蛇王","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
-F = function() provider.db["憤怒使者"]={76,"扛麥郎習近平","鹹魚餅"} end F()
-F = function() provider.db["水晶之刺"]={80,"超人好識飛"} end F()
+F = function() provider.db["世界之樹"]={76,"一塵世繁華","冬季限定丶","熊宝","葉帥","蘇荏苒","阿伍"} end F()
+F = function() provider.db["憤怒使者"]={88,"扛麥郎習近平","鹹魚餅"} end F()
+F = function() provider.db["水晶之刺"]={92,"超人好識飛"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
