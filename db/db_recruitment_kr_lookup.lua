46818a484ce512e3f3aa7b478c3d991600a7f0dc
Jah Raphael <jahraphael@yahoo.com>
2023-01-30 07:15:41 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-01-29T07:07:39Z",numCharacters=34192,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2023-01-30T07:08:15Z",numCharacters=34492,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,22 +2,20 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-01-29T07:07:39Z",numCharacters=34192,db={}}
+local provider={name=...,data=3,region="tw",date="2023-01-30T07:08:15Z",numCharacters=34492,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Deronospala","三杯老鼠肉","凝音","流砂"} end F()
 F = function() provider.db["尖石"]={8,"哲哲","小弟射歪了","小弟崛起了","小弟生氣了","松露焗烤龍蝦"} end F()
 F = function() provider.db["寒冰皇冠"]={18,"一籃雞蛋","之翼你太美","牛牛牌沙茶醬"} end F()
 F = function() provider.db["語風"]={24,"Driveshaft","幽蘭花","老实人","花媽狠美膩"} end F()
-F = function() provider.db["阿薩斯"]={32,"四葉未來","沒有你的未來"} end F()
-F = function() provider.db["眾星之子"]={36,"舞天曦"} end F()
-F = function() provider.db["日落沼澤"]={38,"Cycdragon","Killergirlzz","Killerkingz","Php","一條龍服務","冰火雙重天","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
-F = function() provider.db["世界之樹"]={64,"一塵世繁華","冬季限定丶","斗魚丶葉若晴","熊宝","蘇荏苒","阿伍"} end F()
-F = function() provider.db["地獄吼"]={76,"Dxz"} end F()
-F = function() provider.db["聖光之願"]={78,"Fistpanda","脂肪元素使"} end F()
-F = function() provider.db["米奈希爾"]={82,"Sanjeev"} end F()
-F = function() provider.db["冰風崗哨"]={84,"Morii","腦闊疼"} end F()
-F = function() provider.db["屠魔山谷"]={88,"龍風"} end F()
+F = function() provider.db["阿薩斯"]={32,"四葉未來","沒有你的未來","珍珠幻象"} end F()
+F = function() provider.db["眾星之子"]={38,"舞天曦"} end F()
+F = function() provider.db["日落沼澤"]={40,"Cycdragon","Killergirlzz","Killerkingz","Php","一條龍服務","冰火雙重天","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
+F = function() provider.db["世界之樹"]={66,"一塵世繁華","冬季限定丶","斗魚丶葉若晴","熊宝","蘇荏苒","阿伍"} end F()
+F = function() provider.db["米奈希爾"]={78,"Sanjeev"} end F()
+F = function() provider.db["冰風崗哨"]={80,"Morii","腦闊疼"} end F()
+F = function() provider.db["屠魔山谷"]={84,"龍風"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
