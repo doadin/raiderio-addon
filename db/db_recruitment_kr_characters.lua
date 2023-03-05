9d0d2f2ee6f1ff3844fdfdecdc437f4bf0c20566
Jah Raphael <jahraphael@yahoo.com>
2023-03-05 08:53:56 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-03-04T07:57:12Z",numCharacters=38379,db={}}
+local provider={name=...,data=3,region="kr",date="2023-03-05T08:45:23Z",numCharacters=38110,db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"Killix","니스티르","션리","이터누스"} end F()
+F = function() provider.db["아즈샤라"]={0,"Killix","Scarlety","니스티르","닉카","션리","이터누스"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-03-04T07:57:12Z",numCharacters=38379,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2023-03-05T08:45:23Z",numCharacters=38110,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 8
-F = function() provider.lookup[1] = "\7\4\7\4\7\16\7\4" end F()
+-- chunk size: 12
+F = function() provider.lookup[1] = "\7\4s\29\7\4s\29\7\16\7\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
