9eab8c9ad948da6581ebecb47f3d0e925a429035
Jah Raphael <jahraphael@yahoo.com>
2023-03-10 08:07:37 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-03-09T07:58:18Z",numCharacters=38264,db={}}
+local provider={name=...,data=3,region="tw",date="2023-03-10T07:58:47Z",numCharacters=38421,db={}}
 local F
 
 F = function() provider.db["世界之樹"]={0,"Marb","Minio","Ogre","Richmal","一塵世繁華","不会开嗜血","全球肉眼帝","冬季限定丶","刃舞丨迦楼罗","封城牛侠","斗魚丶葉若晴","灬老头乐灬","熊宝","聆听秋雨","萨摩耶","阿伍","风痕磨石"} end F()
