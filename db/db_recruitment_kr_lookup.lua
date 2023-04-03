0398edf180c6bc8f47f45676a0661f6397612555
Jah Raphael <jahraphael@yahoo.com>
2023-04-03 08:54:50 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-04-02T08:35:33Z",numCharacters=32331,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2023-04-03T08:36:04Z",numCharacters=32165,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 4
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-02T08:35:33Z",numCharacters=32331,db={}}
+local provider={name=...,data=3,region="tw",date="2023-04-03T08:36:04Z",numCharacters=32165,db={}}
 local F
 
 F = function() provider.db["日落沼澤"]={0,"一條龍服務","冰火雙重天","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","花殤紫幽幽","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
-F = function() provider.db["雷鱗"]={20,"Areher","Jierax","万万兮","久夕","喵呜","國家电网","大魔怔","子龍","沒有魚丸","猪九平一","瑞娅","皮卡丘","等待","红尾鵟","群星萝莉","背上有翅膀","這不對吧"} end F()
+F = function() provider.db["雷鱗"]={20,"Areher","Df","Jierax","万万兮","久夕","喵呜","國家电网","大魔怔","沒有魚丸","猪九平一","瑞娅","皮卡丘","等待","红尾鵟","群星萝莉","背上有翅膀","這不對吧"} end F()
 F = function() provider.db["世界之樹"]={54,"Minio","Richmal","不会开嗜血","丨渣渣丶術丨","全球肉眼帝","封城牛侠","灬老头乐灬","聆听秋雨","萨摩耶","风痕磨石"} end F()
 F = function() provider.db["眾星之子"]={74,"Verapamil","伍棍","酸菜菜"} end F()
 F = function() provider.db["暗影之月"]={80,"Bakemono","Maievg","云宝黛西","芬达"} end F()
@@ -14,6 +14,7 @@ F = function() provider.db["銀翼要塞"]={88,"Darkknight","Meigi"} end F()
 F = function() provider.db["米奈希爾"]={92,"雪鹽焦糖貓"} end F()
 F = function() provider.db["水晶之刺"]={94,"潘鳳"} end F()
 F = function() provider.db["阿薩斯"]={96,"劍心","龍心"} end F()
+F = function() provider.db["寒冰皇冠"]={100,"善逸","時透無一郎"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
