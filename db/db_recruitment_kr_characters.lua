a221e7ed6d5c751bd16ad422011faa46d97eec31
Jah Raphael <jahraphael@yahoo.com>
2023-04-06 08:19:23 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-04-05T08:09:03Z",numCharacters=32454,db={}}
+local provider={name=...,data=3,region="kr",date="2023-04-06T08:10:54Z",numCharacters=32567,db={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"Scarlety","닉카"} end F()
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,19 +2,19 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-05T08:09:03Z",numCharacters=32454,db={}}
+local provider={name=...,data=3,region="tw",date="2023-04-06T08:10:54Z",numCharacters=32567,db={}}
 local F
 
 F = function() provider.db["日落沼澤"]={0,"一條龍服務","冰火雙重天","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","花殤紫幽幽","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
-F = function() provider.db["雷鱗"]={20,"Areher","Df","Jierax","万万兮","久夕","喵呜","國家电网","大魔怔","沒有魚丸","猪九平一","瑞娅","皮卡丘","等待","红尾鵟","群星萝莉","背上有翅膀","這不對吧"} end F()
-F = function() provider.db["世界之樹"]={54,"Richmal","灬老头乐灬"} end F()
-F = function() provider.db["眾星之子"]={58,"Verapamil","伍棍","酸菜菜"} end F()
-F = function() provider.db["暗影之月"]={64,"Bakemono","Maievg","云宝黛西"} end F()
-F = function() provider.db["銀翼要塞"]={70,"Darkknight","Meigi"} end F()
-F = function() provider.db["米奈希爾"]={74,"雪鹽焦糖貓"} end F()
-F = function() provider.db["水晶之刺"]={76,"潘鳳"} end F()
-F = function() provider.db["阿薩斯"]={78,"劍心","龍心"} end F()
-F = function() provider.db["寒冰皇冠"]={82,"善逸","時透無一郎"} end F()
+F = function() provider.db["雷鱗"]={20,"Areher","Df","Jierax","万万兮","久夕","喵呜","國家电网","大魔怔","沒有魚丸","猪九平一","瑞娅","皮卡丘","等待","红尾鵟","群星萝莉","背上有翅膀","赞美太阳","這不對吧"} end F()
+F = function() provider.db["世界之樹"]={56,"Richmal","灬老头乐灬"} end F()
+F = function() provider.db["眾星之子"]={60,"Verapamil","伍棍","酸菜菜"} end F()
+F = function() provider.db["暗影之月"]={66,"Bakemono","Maievg","云宝黛西"} end F()
+F = function() provider.db["銀翼要塞"]={72,"Darkknight","Meigi"} end F()
+F = function() provider.db["米奈希爾"]={76,"雪鹽焦糖貓"} end F()
+F = function() provider.db["水晶之刺"]={78,"潘鳳"} end F()
+F = function() provider.db["阿薩斯"]={80,"劍心","龍心"} end F()
+F = function() provider.db["寒冰皇冠"]={84,"善逸","時透無一郎"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
