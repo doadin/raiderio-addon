8022d21ae8e87808666431ba46cd6d3684cf6bf4
Jah Raphael <jahraphael@yahoo.com>
2023-04-08 08:16:41 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-07T08:14:23Z",numCharacters=32035,db={}}
+local provider={name=...,data=3,region="tw",date="2023-04-08T08:08:18Z",numCharacters=31706,db={}}
 local F
 
 F = function() provider.db["日落沼澤"]={0,"一條龍服務","冰火雙重天","我爱看烟花","灵犀","無尾熊之星","肉嘟嘟胖呼呼","花殤紫幽幽","阿戦","阿莱克斯莉萨","非正义的圣光"} end F()
@@ -10,11 +10,10 @@ F = function() provider.db["雷鱗"]={20,"Areher","Df","Jierax","万万兮","久
 F = function() provider.db["世界之樹"]={56,"Richmal","灬老头乐灬"} end F()
 F = function() provider.db["眾星之子"]={60,"Verapamil","伍棍","酸菜菜"} end F()
 F = function() provider.db["暗影之月"]={66,"Bakemono","Maievg","云宝黛西"} end F()
-F = function() provider.db["銀翼要塞"]={72,"Darkknight","Meigi"} end F()
-F = function() provider.db["米奈希爾"]={76,"雪鹽焦糖貓"} end F()
-F = function() provider.db["水晶之刺"]={78,"潘鳳"} end F()
-F = function() provider.db["阿薩斯"]={80,"劍心","龍心"} end F()
-F = function() provider.db["寒冰皇冠"]={84,"善逸","時透無一郎"} end F()
+F = function() provider.db["米奈希爾"]={72,"雪鹽焦糖貓"} end F()
+F = function() provider.db["水晶之刺"]={74,"潘鳳"} end F()
+F = function() provider.db["阿薩斯"]={76,"劍心","龍心"} end F()
+F = function() provider.db["寒冰皇冠"]={80,"善逸","時透無一郎"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
