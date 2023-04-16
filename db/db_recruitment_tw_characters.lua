fb24e29ea8e120004e26e857e9b9550e5e8448ac
Jah Raphael <jahraphael@yahoo.com>
2023-04-16 08:15:59 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-15T08:11:29Z",numCharacters=31648,db={}}
+local provider={name=...,data=3,region="tw",date="2023-04-16T08:07:23Z",numCharacters=31490,db={}}
 local F
 
 F = function() provider.db["亞雷戈斯"]={0,"穢邪路路","聖光路路"} end F()
@@ -10,9 +10,9 @@ F = function() provider.db["世界之樹"]={4,"Paoda","Richmal","灬老头乐灬
 F = function() provider.db["冰霜之刺"]={14,"七轟"} end F()
 F = function() provider.db["雷鱗"]={16,"Areher","Jierax","万万兮","久夕","喵呜","國家电网","大魔怔","沒有魚丸","猪九平一","瑞娅","皮卡丘","等待","红尾鵟","群星萝莉","背上有翅膀","赞美太阳","這不對吧"} end F()
 F = function() provider.db["眾星之子"]={50,"Verapamil","伍棍","酸菜菜"} end F()
-F = function() provider.db["暗影之月"]={56,"Bakemono","Maievg","云宝黛西"} end F()
-F = function() provider.db["寒冰皇冠"]={62,"善逸","時透無一郎"} end F()
-F = function() provider.db["水晶之刺"]={66,"潘鳳"} end F()
+F = function() provider.db["暗影之月"]={56,"Bakemono","Maievg"} end F()
+F = function() provider.db["寒冰皇冠"]={60,"善逸","時透無一郎"} end F()
+F = function() provider.db["水晶之刺"]={64,"潘鳳"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-15T08:11:29Z",numCharacters=31648,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-04-16T08:07:23Z",numCharacters=31490,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 68
-F = function() provider.lookup[1] = "?\29?\29?\29?\13?\13?\29?\29?\29?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13;\16;\4;\4;\16" end F()
+-- chunk size: 66
+F = function() provider.lookup[1] = "?\29?\29?\29?\13?\13?\29?\29?\29?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13?\13;\4;\4;\16" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
