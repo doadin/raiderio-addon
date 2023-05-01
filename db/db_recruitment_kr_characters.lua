bf0fc42be69a7026057813c72f97720568181873
Jah Raphael <jahraphael@yahoo.com>
2023-05-01 07:33:17 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-04-30T07:26:50Z",numCharacters=32089,db={}}
+local provider={name=...,data=3,region="kr",date="2023-05-01T07:25:43Z",numCharacters=32531,db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"Scarlety","닉카"} end F()
+F = function() provider.db["아즈샤라"]={0,"Maxava","Maxxava"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-04-30T07:26:50Z",numCharacters=32089,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-05-01T07:25:43Z",numCharacters=32531,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 54
