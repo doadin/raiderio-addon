ae875d885ad1638e3994b1e249e31eae913b2d35
Jah Raphael <jahraphael@yahoo.com>
2023-05-06 08:56:23 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-05-04T07:26:59Z",numCharacters=32355,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-05-06T08:49:25Z",numCharacters=32691,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 46
-F = function() provider.lookup[1] = "?\29;\8?\29?\29?\29?\29?\13?\13?\13?\13?\13?\13s\29\4\4s\29\4\4s\29?\13?\13?\13?\13;\16;\4" end F()
+F = function() provider.lookup[1] = "?\29\4\4;\8?\29?\29?\29?\13?\13?\13?\13?\13?\13s\29\4\4C\29s\29?\13?\13?\13?\13?\13;\16;\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
