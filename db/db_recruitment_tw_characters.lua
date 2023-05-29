5e4ea04816868d00913798ff8830e91c34bd77d5
Jah Raphael <jahraphael@yahoo.com>
2023-05-29 08:56:30 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-05-28T08:50:38Z",numCharacters=34408,db={}}
+local provider={name=...,data=3,region="tw",date="2023-05-29T08:52:20Z",numCharacters=34357,db={}}
 local F
 
 F = function() provider.db["阿薩斯"]={0,"惡魔腳毛","漆黑腳毛","灵魂复苏","蜜雪","黑龍腳毛"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-05-28T08:50:38Z",numCharacters=34408,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-05-29T08:52:20Z",numCharacters=34357,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 76
