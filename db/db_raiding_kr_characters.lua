58f1c4339c1147b7e0f70f9a50d6cec83784ed67
Jah Raphael <jahraphael@yahoo.com>
2023-06-17 08:15:43 +0000

    [Raider.IO] Database Refresh

--- db/db_raiding_kr_characters.lua
+++ db/db_raiding_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={["name"]=...,["data"]=2,["region"]="kr",["date"]="2023-06-16T07:19:29Z",["numCharacters"]=186,["recordSizeInBytes"]=11,["encodingOrder"]={1,2,3,4},["currentRaids"]={{["id"]=14663,["mapId"]=2569,["name"]="Aberrus, the Shadowed Crucible",["shortName"]="ATSC",["bossCount"]=9,["ordinal"]=1}},["previousRaids"]={{["id"]=14030,["mapId"]=2522,["name"]="Vault of the Incarnates",["shortName"]="VOTI",["bossCount"]=8,["ordinal"]=2}},["db"]={}}
+local provider={["name"]=...,["data"]=2,["region"]="kr",["date"]="2023-06-17T07:22:10Z",["numCharacters"]=186,["recordSizeInBytes"]=11,["encodingOrder"]={1,2,3,4},["currentRaids"]={{["id"]=14663,["mapId"]=2569,["name"]="Aberrus, the Shadowed Crucible",["shortName"]="ATSC",["bossCount"]=9,["ordinal"]=1}},["previousRaids"]={{["id"]=14030,["mapId"]=2522,["name"]="Vault of the Incarnates",["shortName"]="VOTI",["bossCount"]=8,["ordinal"]=2}},["db"]={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"Jete","Karmin","Korean","Punishment","Satur","검든소","고스트페이스킬러","귀여운복술","김오제","내생석금생석","단칼에샤샥","더블트러블","데샨","디밋취취","떼떼","레미","루린","리퀴드스워드","마리시에","만나보고싶다","말랑말랑소","무회전킥전문","미디님","밀레이","박섀로이","발리아나","뿌악용","새군","새러","새러드루","새러법사","새러벤저","새러홀릭","세나","세라","세리비안","세설","수도요","숨참고닥딜","숨참고빡딜","스피드런","신박요","쑤옌","아구빡새","아세리나","아이레니안","아이레드레용","아이레시안","아이레이나","아이레이아","아이세이나","아이에니아","아이에리아","아임히어로딩동댕","앗메리카노","야폼","엔터더우탱","와길동","용다리살","우탱킬러비","이앗아","정신나간채찍","제떼","주취","징기즈칸","쯔위님님","카렐","쿡쿡다스","쿡하는세상","크지","크징","큰별","키우지마","토루샨","파넬로페","파맛심판","파맛토템","파이브베놈","폭풍의아크","하로운","하은","핼랑","헬퍼","활활타오른다","흐쿡","히아린"} end F()
