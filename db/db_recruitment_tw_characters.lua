46e44664f3c1f2a19c3fe532b4d9c2dcecd3e983
Jah Raphael <jahraphael@yahoo.com>
2023-06-30 09:19:16 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-06-29T08:58:26Z",numCharacters=31501,db={}}
+local provider={name=...,data=3,region="tw",date="2023-06-30T09:09:20Z",numCharacters=31377,db={}}
 local F
 
 F = function() provider.db["阿薩斯"]={0,"惡魔腳毛","漆黑腳毛","蜜雪","黑龍腳毛"} end F()
 F = function() provider.db["世界之樹"]={8,"Paoda","菜菜子","阿提米斯"} end F()
-F = function() provider.db["亞雷戈斯"]={14,"燃燒路路","穢邪路路"} end F()
-F = function() provider.db["冰霜之刺"]={18,"七轟","毀滅的花兒"} end F()
-F = function() provider.db["暗影之月"]={22,"Nazhock","Worchief","巨獸斬","狼魅妹"} end F()
-F = function() provider.db["水晶之刺"]={30,"潘鳳"} end F()
-F = function() provider.db["寒冰皇冠"]={32,"代購男子漢"} end F()
+F = function() provider.db["亞雷戈斯"]={14,"穢邪路路"} end F()
+F = function() provider.db["冰霜之刺"]={16,"七轟","毀滅的花兒"} end F()
+F = function() provider.db["暗影之月"]={20,"Nazhock","Worchief","巨獸斬","狼魅妹"} end F()
+F = function() provider.db["水晶之刺"]={28,"潘鳳"} end F()
+F = function() provider.db["寒冰皇冠"]={30,"代購男子漢"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-06-29T08:58:26Z",numCharacters=31501,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-06-30T09:09:20Z",numCharacters=31377,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 34
-F = function() provider.lookup[1] = "?\13?\13?\13?\13?\29?\29?\29?\29?\29?\29?\29t\29t\29t\29t\29;\16;\4" end F()
+-- chunk size: 32
+F = function() provider.lookup[1] = "?\13?\13?\13?\13?\29?\29?\29?\29?\29?\29t\29t\29t\29t\29;\16;\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
