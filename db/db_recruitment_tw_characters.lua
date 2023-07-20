e4b1dcb98d6fa91fb67d64730c8b908966098207
Jah Raphael <jahraphael@yahoo.com>
2023-07-20 09:25:10 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-07-19T09:09:18Z",numCharacters=29736,db={}}
+local provider={name=...,data=3,region="tw",date="2023-07-20T09:14:40Z",numCharacters=29634,db={}}
 local F
 
 F = function() provider.db["亞雷戈斯"]={0,"穢邪路路","路路利拉"} end F()
@@ -10,7 +10,6 @@ F = function() provider.db["世界之樹"]={4,"Paoda","菜菜子","阿提米斯"
 F = function() provider.db["冰霜之刺"]={10,"七轟","千樹彥","毀滅的花兒"} end F()
 F = function() provider.db["克羅之刃"]={16,"Readmeinname","一根大龙北","亚煞极","尤格萨隆","微醺灬思慕术","貓叔的小鈴鐺"} end F()
 F = function() provider.db["冰風崗哨"]={28,"Novem","Novemmonk"} end F()
-F = function() provider.db["暗影之月"]={32,"Nazhock","Worchief","巨獸斬","狼魅妹"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
