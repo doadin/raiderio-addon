aa85f0f233fd34598f7153b1064176b1d6ba2d5c
Jah Raphael <jahraphael@yahoo.com>
2023-07-27 09:07:14 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,15 +2,15 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-07-26T08:55:09Z",numCharacters=29136,db={}}
+local provider={name=...,data=3,region="tw",date="2023-07-27T08:56:41Z",numCharacters=28957,db={}}
 local F
 
 F = function() provider.db["亞雷戈斯"]={0,"穢邪路路","路路利拉"} end F()
 F = function() provider.db["世界之樹"]={4,"Paoda","菜菜子","阿提米斯"} end F()
 F = function() provider.db["冰霜之刺"]={10,"七轟","千樹彥","毀滅的花兒"} end F()
-F = function() provider.db["克羅之刃"]={16,"Readmeinname","一根大龙北","亚煞极","尤格萨隆","微醺灬思慕术","貓叔的小鈴鐺"} end F()
-F = function() provider.db["冰風崗哨"]={28,"Novem","Novemmonk"} end F()
-F = function() provider.db["狂熱之刃"]={32,"泷泷龙泷泷"} end F()
+F = function() provider.db["克羅之刃"]={16,"Readmeinname","一根大龙北","尤格萨隆","微醺灬思慕术","貓叔的小鈴鐺"} end F()
+F = function() provider.db["冰風崗哨"]={26,"Novem","Novemmonk"} end F()
+F = function() provider.db["狂熱之刃"]={30,"泷泷龙泷泷"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-07-26T08:55:09Z",numCharacters=29136,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-07-27T08:56:41Z",numCharacters=28957,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 34
-F = function() provider.lookup[1] = "?\29?\29?\29?\29?\29?\29?\29?\29?\13?\13?\13?\13?\13?\13<\13<\13;\4" end F()
+-- chunk size: 32
+F = function() provider.lookup[1] = "?\29?\29?\29?\29?\29?\29?\29?\29?\13?\13?\13?\13?\13<\13<\13;\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
