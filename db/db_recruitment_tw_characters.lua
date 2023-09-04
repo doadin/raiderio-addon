e18825c09d82df899eb5bc79e6772cf927c59edb
Jah Raphael <jahraphael@yahoo.com>
2023-09-04 08:18:23 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-09-03T08:13:22Z",numCharacters=25174,db={}}
+local provider={name=...,data=3,region="tw",date="2023-09-04T08:12:36Z",numCharacters=25181,db={}}
 local F
 
 F = function() provider.db["屠魔山谷"]={0,"伊力丹一怒風"} end F()
