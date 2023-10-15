8fcfecec1d1c845d2dabe8045bb52c5447041a7e
Jah Raphael <jahraphael@yahoo.com>
2023-10-15 08:45:34 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-10-14T08:25:50Z",numCharacters=23206,db={}}
+local provider={name=...,data=3,region="kr",date="2023-10-15T08:34:56Z",numCharacters=22907,db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"드루가버렷","따귀요정","민뤼","민초단뿡이","성기사할만해","오옹나힐순","으시안오옹","해오름뿡이"} end F()
+F = function() provider.db["아즈샤라"]={0,"드루가버렷","민뤼","민초단뿡이","성기사할만해","오옹나힐순","으시안오옹","해오름뿡이"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-10-14T08:25:50Z",numCharacters=23206,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2023-10-15T08:34:56Z",numCharacters=22907,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 16
-F = function() provider.lookup[1] = ";\8;\4;\4;\8;\8;\8;\4;\8" end F()
+-- chunk size: 14
+F = function() provider.lookup[1] = ";\8;\4;\8;\8;\8;\4;\8" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
