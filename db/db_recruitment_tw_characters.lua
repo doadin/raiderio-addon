d8edcdf2009d0a9ade97cf66dac017673edff3fe
Jah Raphael <jahraphael@yahoo.com>
2023-10-16 08:33:19 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-10-15T08:34:56Z",numCharacters=22907,db={}}
+local provider={name=...,data=3,region="tw",date="2023-10-16T08:24:02Z",numCharacters=22849,db={}}
 local F
 
 F = function() provider.db["寒冰皇冠"]={0,"Ulmoo"} end F()
