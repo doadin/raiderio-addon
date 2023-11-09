fd8040f673758c9c0e498a5fd745abbc7492c844
Jah Raphael <jahraphael@yahoo.com>
2023-11-09 08:55:32 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-11-08T08:42:09Z",numCharacters=27762,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-11-09T08:49:13Z",numCharacters=27919,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 80
-F = function() provider.lookup[1] = "t\29t\29\8\4t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29" end F()
+-- chunk size: 78
+F = function() provider.lookup[1] = "t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29t\29" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
