f850037439bb6f6371070d69e361588952ae6cff
Jah Raphael <jahraphael@yahoo.com>
2023-11-10 08:52:10 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-11-09T08:49:13Z",numCharacters=27919,db={}}
+local provider={name=...,data=3,region="tw",date="2023-11-10T08:45:47Z",numCharacters=28187,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"聖誕","領主"} end F()
