47b6f18e74370235461d4a2382a09b79e24939a0
Jah Raphael <jahraphael@yahoo.com>
2023-11-26 08:48:11 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-11-25T08:42:51Z",numCharacters=30198,db={}}
+local provider={name=...,data=3,region="tw",date="2023-11-26T08:43:07Z",numCharacters=30323,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Momococo","Tall","低排放載具","吃土更健康","地獄吹水","大叔和理非","天線低低嗶","小龍公主","惡魔小妹妹","浪潮披薩","減弱薩滿","科興大師兄","米拉妮","聖誕","芋泥","阿耳忒弥斯","領主","黑魔后"} end F()
