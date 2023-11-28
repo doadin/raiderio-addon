efd44b1234ac480ad8688789e994201f46dc44e7
Jah Raphael <jahraphael@yahoo.com>
2023-11-28 08:47:13 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-11-27T08:42:21Z",numCharacters=30625,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-11-28T08:41:57Z",numCharacters=31145,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 114
